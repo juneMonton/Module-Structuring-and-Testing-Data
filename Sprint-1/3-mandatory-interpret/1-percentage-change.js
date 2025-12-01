fd67af9f5d4e9758d0cf5effc687f7dbd02366d3
JuneMonton <junemonton@JuneMontons-MacBook-Air.local>
2025-12-01 20:55:04 +0100

    docs: add step-by-step explanations for the code with comments

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -13,10 +13,33 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+    /*
+    There are 5 function calls,
+    line 4 carPrice.replaceAll()
+    line 4 Number()
+    line 5 priceAfterOneYear.replaceAll()
+    line 5 Number()
+    line 10 console.log()
+    */
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+    /* There's a syntax error in line 5, there's no comma in between arguments
+    it should be priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+    */
+
 
 // c) Identify all the lines that are variable reassignment statements
 
+    /*There are 2 variable reassignments
+    line 4, carPrice = Number(carPrice.replaceAll(",", ""));
+    line 5, priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+
 // d) Identify all the lines that are variable declarations
 
+    /*Variable Declarations are lines 1,2,7 and 8
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+    //It removes all the commas converting the string to a number so it can be used in the math operations
+    
--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -13,13 +13,26 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
-// b) How many function calls are there?
+    /*
+    There are 6 variable declarations
+    const movieLength
+    const remainingSeconds
+    const totalMinutes
+    const remainingMinutes
+    const totalHours
+    const result
+    */
+
+// b) How many function calls are there?-There's only 1 function call
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+    //The expression  movieLength%60 represents modulo operator, using %60 helps you extract how many seconds remaining.
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+    //totalMinutes represents how many complete minutes fit inside the movie length.
 
-// e) What do you think the variable result represents? Can you think of a better name for this variable?
+// e) What do you think the variable result represents? Can you think of a better name for this variable?-the result variable formats the movie time to a string showing hours, minutes and seconds, it can be renamed to formattedTime
 
-// f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer-Yes as long as the movieLength value is positive
