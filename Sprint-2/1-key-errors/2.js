5e5afee2abe41ee0f1f10c7c4e1ff0dc49676025
JuneMonton <junemonton@JuneMontons-MacBook-Air.local>
2025-12-16 15:06:04 +0100

    docs: answered and performed tasks in 1-key-errors 2

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,22 @@
 
 // Predict and explain first BEFORE you run any code...
-
+//this will not run because variables num are not declared
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> write your prediction of the error here: num is not defined
 
 function square(3) {
     return num * num;
 }
 
-// =============> write the error message here
+// =============> write the error message here: " Illegal return statement"
 
-// =============> explain this error message here
+// =============> explain this error message here: The return is outside the function, Javascript is treating is at function since the parameter is a value and not an identifier that's why the return inside was not treated as a function
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+    return num * num;
+}
