98258481a6c4ebc271a759a404899388001ce4e4
JuneMonton <junemonton@JuneMontons-MacBook-Air.local>
2025-12-22 21:39:52 +0100

    docs: added answer code to Sprint 3 2-mandatory-rewrite and 3-mandatory-practice

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,15 @@
 function getOrdinalNumber(num) {
- return "1st";
+ // Handle 11, 12, 13 as special cases
+    if (num % 100 >= 11 && num % 100 <= 13) {
+        return num + "th";
+    }
+
+    // Determine suffix based on last digit
+    const lastDigit = num % 10;
+    if (lastDigit === 1) return num + "st";
+    if (lastDigit === 2) return num + "nd";
+    if (lastDigit === 3) return num + "rd";
+    return num + "th";
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,9 @@
-function repeat() {
-    return "hellohellohello";
+function repeat(str,count) {
+    if (count < 0) {
+        throw new Error("Count cannot be negative");
+    }
+    if (count === 0) return "";
+    return str.repeat(count);
 }
 
 module.exports = repeat;
